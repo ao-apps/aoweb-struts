67b8e6747e06ffde7e4b9a4d7d47616feddf4c74
Dan Armstrong <dan@aoindustries.com>
2013-03-20 20:05:44 +0000

    Apache concurrency in AOServ now

--- WEB-INF/classes/com/aoindustries/website/clientarea/control/password/PasswordGeneratorAction.java
+++ WEB-INF/classes/com/aoindustries/website/clientarea/control/password/PasswordGeneratorAction.java
@@ -1,7 +1,7 @@
 package com.aoindustries.website.clientarea.control.password;
 
 /*
- * Copyright 2007-2011 by AO Industries, Inc.,
+ * Copyright 2007-2009 by AO Industries, Inc.,
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
@@ -11,6 +11,7 @@ import com.aoindustries.website.SiteSettings;
 import com.aoindustries.website.Skin;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.ActionForm;
@@ -33,6 +34,7 @@ public class PasswordGeneratorAction extends HttpsAction {
         HttpServletRequest request,
         HttpServletResponse response,
         SiteSettings siteSettings,
+        Locale locale,
         Skin skin
     ) throws Exception {
         // Generate the passwords
