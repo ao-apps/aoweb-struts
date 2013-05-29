bb3e240ad33dde13a1153d6181c254b2560f3014
Dan Armstrong <dan@aoindustries.com>
2013-05-29 15:28:50 +0000

    Moving production to head

--- WEB-INF/classes/com/aoindustries/website/signup/Managed2Action.java
+++ WEB-INF/classes/com/aoindustries/website/signup/Managed2Action.java
@@ -1,12 +1,13 @@
 package com.aoindustries.website.signup;
 
 /*
- * Copyright 2007-2011 by AO Industries, Inc.,
+ * Copyright 2007-2009 by AO Industries, Inc.,
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
 import com.aoindustries.website.SiteSettings;
 import com.aoindustries.website.Skin;
+import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.ActionForward;
@@ -23,6 +24,7 @@ public class Managed2Action extends ManagedStepAction {
         HttpServletRequest request,
         HttpServletResponse response,
         SiteSettings siteSettings,
+        Locale locale,
         Skin skin,
         ManagedSignupSelectPackageForm signupSelectPackageForm,
         boolean signupSelectPackageFormComplete,
