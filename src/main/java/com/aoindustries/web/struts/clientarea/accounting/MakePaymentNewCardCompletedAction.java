8004c94e36c07e2580b99f4a52354c447407c7c8
AO Industries, Inc <support@aoindustries.com>
2025-03-10 08:24:55 -0500

    TODO: 303 redirect after all payment (and attempts).  A client double-paid with an accidental refresh.

--- src/main/java/com/aoindustries/web/struts/clientarea/accounting/MakePaymentNewCardCompletedAction.java
+++ src/main/java/com/aoindustries/web/struts/clientarea/accounting/MakePaymentNewCardCompletedAction.java
@@ -1,6 +1,6 @@
 /*
  * aoweb-struts - Template webapp for legacy Struts-based site framework with AOServ Platform control panels.
- * Copyright (C) 2007-2009, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2024  AO Industries, Inc.
+ * Copyright (C) 2007-2009, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2024, 2025  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -60,6 +60,8 @@ import org.apache.struts.action.ActionMessages;
 
 /**
  * Payment from stored credit card.
+ *
+ * <p>TODO: 303 redirect after all payment (and attempts).  A client double-paid with an accidental refresh.</p>
  *
  * @author  AO Industries, Inc.
  */
