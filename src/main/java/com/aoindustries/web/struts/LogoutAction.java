0c035d38fab1f8d017dc3301700afeec77420970
AO Industries, Inc <support@aoindustries.com>
2025-12-28 15:50:43 -0600

    Use non-deprecated package for Struts 2

--- src/main/java/com/aoindustries/web/struts/LogoutAction.java
+++ src/main/java/com/aoindustries/web/struts/LogoutAction.java
@@ -1,6 +1,6 @@
 /*
  * aoweb-struts - Template webapp for legacy Struts-based site framework with AOServ Platform control panels.
- * Copyright (C) 2007-2009, 2016, 2019, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2007-2009, 2016, 2019, 2021, 2022, 2025  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -24,10 +24,10 @@
 package com.aoindustries.web.struts;
 
 import com.aoapps.net.URIEncoder;
-import com.opensymphony.xwork2.ActionSupport;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.apache.struts2.ActionSupport;
 import org.apache.struts2.ServletActionContext;
 
 /**
